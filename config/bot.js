795b2ae82d551fefd3f04681b89aed6e4712b27c
Sasori-Samas <77126624+Sasori-Samas@users.noreply.github.com>
2021-01-11 09:57:02 -0500

    Initial commit

---
+++ config/bot.js
@@ -0,0 +1,17 @@
+module.exports = {
+    emojis: {
+        off: ':x:',
+        error: ':warning:',
+        queue: ':bar_chart:',
+        music: ':musical_note:',
+        success: ':white_check_mark:',
+    },
+
+    discord: {
+        token: 'Nzk2MDg1NjA5MDI2Mjg5NjY3.X_Syfw.Vnz7pb_nT-N15ajYEln21sAK1qg',
+        prefix: '+',
+        activity: '+Help/ Add Sasori Sama#0002 for help ! ',
+    },
+
+    filters: ['8D', 'gate', 'haas', 'phaser', 'treble', 'tremolo', 'vibrato', 'reverse', 'karaoke', 'flanger', 'mcompand', 'pulsator', 'subboost', 'bassboost', 'vaporwave', 'nightcore', 'normalizer', 'surrounding'],
+};
