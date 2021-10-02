f51b72d193cd3b3ae26068def11ed7883dc21fe5
Sasori-Samas <77126624+Sasori-Samas@users.noreply.github.com>
2021-10-01 21:20:25 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,41 +1,22 @@
-const fs = require('fs');
-const discord = require('discord.js');
-
-const client = new discord.Client({ disableMentions: 'everyone' });
-
 const { Player } = require('discord-player');
+const { Client, Intents } = require('discord.js');
 
-client.player = new Player(client);
-client.config = require('./config/bot');
-client.emotes = client.config.emojis;
-client.filters = client.config.filters;
-client.commands = new discord.Collection();
-
-fs.readdirSync('./commands').forEach(dirs => {
-    const commands = fs.readdirSync(`./commands/${dirs}`).filter(files => files.endsWith('.js'));
-
-    for (const file of commands) {
-        const command = require(`./commands/${dirs}/${file}`);
-        console.log(`Loading command ${file}`);
-        client.commands.set(command.name.toLowerCase(), command);
-    };
+global.client = new Client({
+    intents: [
+        Intents.FLAGS.GUILDS,
+        Intents.FLAGS.GUILD_MEMBERS,
+        Intents.FLAGS.GUILD_MESSAGES,
+        Intents.FLAGS.GUILD_VOICE_STATES
+    ],
+    disableMentions: 'everyone',
 });
 
-const events = fs.readdirSync('./events').filter(file => file.endsWith('.js'));
-const player = fs.readdirSync('./player').filter(file => file.endsWith('.js'));
+client.config = require('./config');
 
-for (const file of events) {
-    console.log(`Loading discord.js event ${file}`);
-    const event = require(`./events/${file}`);
-    client.on(file.split(".")[0], event.bind(null, client));
-};
+global.player = new Player(client, client.config.opt.discordPlayer);
 
-for (const file of player) {
-    console.log(`Loading discord-player event ${file}`);
-    const event = require(`./player/${file}`);
-    client.player.on(file.split(".")[0], event.bind(null, client));
-};
-666
+require('./src/loader');
+require('./src/events'); 
 
 client.login(client.config.discord.token); 
 
